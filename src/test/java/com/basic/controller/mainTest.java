8941e4b5088bd7ecd9d9563e69db043920c663a9
Tjcug <798750509@qq.com>
2018-03-17 03:12:58 +0800

    Initial commit

---
+++ src/test/java/com/basic/controller/mainTest.java
@@ -0,0 +1,18 @@
+package com.basic.controller;
+
+/**
+ * locate com.basic.controller
+ * Created by mastertj on 2018/3/2.
+ */
+public class mainTest {
+    public static void main(String[] args) {
+        int num1=1;
+        int num2=2;
+        int num3=add(num1,num2);
+        System.out.println(num1+num2);
+    }
+
+    public static int add(int num1,int num2){
+        return num1+num2;
+    }
+}
